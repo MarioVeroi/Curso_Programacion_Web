c5671d395199b445b1dd9041c7b796575e604ed0
Mario Veroi <gamer_dax@yahoo.com>
2025-05-23 04:06:26 -0600

    cambios

--- Curso de Javascript/js/Ejercicio4.js
+++ Curso de Javascript/js/Ejercicio4.js
@@ -1,2 +1,14 @@
-var datos = ["Mario Dev", "Veroi Barahoa", "Honduras", 28, 175];
+//#1
+//var datos = new Array("Carlos Mario", "Veroi Barahona", 28, 175);
 
+
+//2 
+//var datos = ["Mario Dev", "Veroi Barahoa", "Honduras", 28, 175];
+
+//3
+var datos = [];
+datos[0] = "Carlos Mario";
+datos[1] = "Veroi Barahona";
+datos[2] = "Honduras";
+datos[3] = 28;
+datos[4] = 175;
